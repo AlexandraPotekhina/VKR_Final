185bb189c4cc97fac5566e6e3fa2dcaa3fc47595
AlexandraPotekhina <alexandra.potekhina1@gmail.com>
2024-05-24 13:42:42 +0000

    modify & refactor code

--- NN_model_Flask/app.py
+++ NN_model_Flask/app.py
@@ -19,12 +19,6 @@ def predict_value(to_predict_list):
     return result[0]
 
 
-#def mn_prediction(params):
-    #model = tf.keras.models.load_model('NN_model_Flask/saved_model.h5')
-    #pred = model.predict([params])
-    #return pred
-
-
 @app.route('/predict',methods = ['POST'])
 def submit_prediction():
     if request.method == 'POST':
@@ -35,20 +29,6 @@ def submit_prediction():
         prediction = str(result)
         return render_template('predict.html',prediction=prediction)
     
-
-#def mn_predict():
-    #message = ''
-    #if request.method == 'POST':
-        #param_list = ('plot', 'mup', 'ko', 'seg', 'tv', 'pp', 'mup', 'pr', 'ps', 'yn', 'shn', 'pln')
-        #params = []
-        #for i in param_list:
-            #param = request.form.get(i)
-            #params.append(param)
-        #params = [float(i.replace(',', '.')) for i in params]
-
-        #message = f'Спрогнозированное Соотношение матрица-наполнитель для введенных параметров: {mn_prediction(params)}'
-    #return render_template('mn.html', message=message)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
